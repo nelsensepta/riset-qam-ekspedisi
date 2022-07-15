a179543f8395465616f500f4efa4893b80ae900a
Nelsen Septa <nelsenseptahenidar@gmail.com>
2022-07-16 01:40:37 +0700

    deploy 5

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -16,7 +16,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Container from "@mui/material/Container";
 import { NavLink, Link } from "react-router-dom";
 
-import LoginButton from "../LoginButton";
 import LogoutButton from "../LogoutButton";
 const drawerWidth = 240;
 
