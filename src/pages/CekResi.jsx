77b246ad63e049c1f15ad029eb383b33849641dd
Nelsen Septa <nelsenseptahenidar@gmail.com>
2022-07-15 17:09:27 +0700

    Deploy 3

--- src/pages/CekResi.jsx
+++ src/pages/CekResi.jsx
@@ -1,7 +1,6 @@
 import { gql, useSubscription } from "@apollo/client";
 import { ThreeDots } from "react-loader-spinner";
 import TableResi from "../components/TableResi";
-import { Box, Skeleton, Stack } from "@mui/material";
 
 export default function CekResi() {
   const GET_CEK_RESI = gql`
@@ -40,19 +39,6 @@ export default function CekResi() {
   return (
     <>
       {loading ? (
-        // <Stack spacing={2}>
-        //   <Skeleton
-        //     width={150}
-        //     height={50}
-        //     sx={{ borderRadius: "5px" }}
-        //     variant="rectangular"
-        //   />
-        //   <Skeleton height={75} variant="rectangular" />
-        //   <Skeleton height={50} variant="rectangular" />
-        //   <Skeleton height={50} variant="rectangular" />
-        //   <Skeleton height={50} variant="rectangular" />
-        //   <Skeleton height={75} variant="rectangular" />
-        // </Stack>
         <ThreeDots color="#00BFFF" height={100} width={100} />
       ) : (
         <TableResi data={data.cek_resi} headCells={headCells} />
